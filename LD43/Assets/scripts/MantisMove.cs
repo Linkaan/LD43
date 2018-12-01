8aa513ab248aaae02c9caacb598758026504689e
Linus Styrén <linus122xbb@gmail.com>
2018-12-01 17:23:57 +0100

    second prototype done

---
+++ LD43/Assets/scripts/MantisMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantisMove : MonoBehaviour {
+
+    public float scale;
+    public float maxSpeed;
+
+    Rigidbody2D rb;
+    Vector3 initMouse;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        initMouse = Input.mousePosition;
+    }
+
+    void FixedUpdate() {
+        float delta = (Input.mousePosition.x - initMouse.x) * scale;
+        if (Mathf.Abs(delta) > maxSpeed) delta = Mathf.Abs(delta) / delta * maxSpeed;
+        rb.AddForce(Vector3.right * delta);
+    }
+}
---
+++ LD43/Assets/scripts/MaxSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxSpeed : MonoBehaviour {
+
+    public float maxSpeed;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = Vector2.Min(rb.velocity, new Vector2(0, maxSpeed));
+	}
+}
